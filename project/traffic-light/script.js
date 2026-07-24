2887665640b81a48a0fd980073ef367e1924144e
vel-3633f <fujiiakito3633@gmail.com>
2026-07-24 22:34:09 +0900

    ライトを東西南北用意

--- project/traffic-light/script.js
+++ project/traffic-light/script.js
@@ -1,21 +1,23 @@
 class Light {
   constructor() {
     this.color;
-    this.lightElement = document.getElementsByClassName("light");
+    this.eastElement = document.getElementById("east").children;
+    this.westElement = document.getElementById("west").children;;
+    this.northElement = document.getElementById("north").children;;
+    this.southElement = document.getElementById("south").children;;
   }
 
   change() {
-    console.log(this.lightElement);
     setTimeout(() => {
-      this.lightElement[0].style.backgroundColor = "green";
+      this.eastElement[0].style.backgroundColor = "green";
     }, 1000);
     setTimeout(() => {
-      this.lightElement[1].style.backgroundColor = "yellow";
-      this.lightElement[0].style.backgroundColor = "grey";
+      this.eastElement[1].style.backgroundColor = "yellow";
+      this.eastElement[0].style.backgroundColor = "grey";
     }, 2000);
     setTimeout(() => {
-      this.lightElement[2].style.backgroundColor = "red";
-      this.lightElement[1].style.backgroundColor = "grey";
+      this.eastElement[2].style.backgroundColor = "red";
+      this.eastElement[1].style.backgroundColor = "grey";
     }, 3000);
   }
 }
