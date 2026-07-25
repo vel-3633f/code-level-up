33183195e48408d246ac0383ba06d1ba2fb6eebd
vel-3633f <fujiiakito3633@gmail.com>
2026-07-25 11:42:41 +0900

    HTML要素をJS側で作るよう修正

--- project/traffic-light/script.js
+++ project/traffic-light/script.js
@@ -1,25 +1,21 @@
 class Light {
-  constructor() {
-    this.color;
-    this.eastElement = document.getElementById("east").children;
-    this.westElement = document.getElementById("west").children;;
-    this.northElement = document.getElementById("north").children;;
-    this.southElement = document.getElementById("south").children;;
+  constructor(id) {
+    this.divElement = document.createElement("div");
+
+    document.getElementsByClassName("crossing")[0].appendChild(this.divElement);
+    this.divElement.classList.add("traffic-light");
+    this.divElement.id = id;
+
+    for (let i = 0; i < 3; i++) {
+      this.lightDivElement = document.createElement("div");
+      this.lightDivElement.classList.add("light");
+      this.divElement.appendChild(this.lightDivElement);
+    }
   }
 
-  change() {
-    setTimeout(() => {
-      this.eastElement[0].style.backgroundColor = "green";
-    }, 1000);
-    setTimeout(() => {
-      this.eastElement[1].style.backgroundColor = "yellow";
-      this.eastElement[0].style.backgroundColor = "grey";
-    }, 2000);
-    setTimeout(() => {
-      this.eastElement[2].style.backgroundColor = "red";
-      this.eastElement[1].style.backgroundColor = "grey";
-    }, 3000);
-  }
+  change_red() {}
 }
 
-new Light().change();
+class Lights {}
+
+new Light("north");
