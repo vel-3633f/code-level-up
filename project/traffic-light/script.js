dbf5b3ff49b89e6f8b1507ffd6fca99780737c18
vel-3633f <fujiiakito3633@gmail.com>
2026-07-25 12:05:12 +0900

    メソッドで色を変更できるよう修正

--- project/traffic-light/script.js
+++ project/traffic-light/script.js
@@ -1,6 +1,8 @@
 class Light {
   constructor(id) {
     this.divElement = document.createElement("div");
+    this.light = [];
+    this.colors = ["red", "yellow", "green"];
 
     document.getElementsByClassName("crossing")[0].appendChild(this.divElement);
     this.divElement.classList.add("traffic-light");
@@ -9,13 +11,16 @@ class Light {
     for (let i = 0; i < 3; i++) {
       this.lightDivElement = document.createElement("div");
       this.lightDivElement.classList.add("light");
-      this.divElement.appendChild(this.lightDivElement);
+      this.light.push(this.divElement.appendChild(this.lightDivElement));
     }
   }
 
-  change_red() {}
+  change(color) {
+    this.light[this.colors.indexOf(color)].classList.add(color);
+  }
 }
 
 class Lights {}
 
 new Light("north");
+new Light("north").change("yellow");
