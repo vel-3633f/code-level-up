ab2a911efd092ef2c9a1ed7749490d82c032ce76
vel-3633f <fujiiakito3633@gmail.com>
2026-07-26 13:09:11 +0900

    コメント化

--- project/oreilly-study/script.js
+++ project/oreilly-study/script.js
@@ -1,31 +1,33 @@
-class Range {
-  constructor(from, to) {
-    this.from = from;
-    this.to = to;
-  }
 
-  includes(x) {
-    return this.from <= x && this.to >= x;
-  }
 
-  toString() {
-    return `(${this.from},${this.to})`;
-  }
-}
+// class Range {
+//   constructor(from, to) {
+//     this.from = from;
+//     this.to = to;
+//   }
 
-class Span extends Range {
-  constructor(start, length) {
-    if (length >= 0) {
-      super(start, start + length);
-    } else {
-      super(start + length, start);
-    }
-  }
-}
+//   includes(x) {
+//     return this.from <= x && this.to >= x;
+//   }
 
-let r = new Range(1, 2);
-let n = new Span(1, 2);
-console.log(n.includes(2));
+//   toString() {
+//     return `(${this.from},${this.to})`;
+//   }
+// }
+
+// class Span extends Range {
+//   constructor(start, length) {
+//     if (length >= 0) {
+//       super(start, start + length);
+//     } else {
+//       super(start + length, start);
+//     }
+//   }
+// }
+
+// let r = new Range(1, 2);
+// let n = new Span(1, 2);
+// console.log(n.includes(2));
 
 // 糖衣構文 以下例
 // let x = 0;
