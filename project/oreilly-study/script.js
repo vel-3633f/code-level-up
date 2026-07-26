4e4dde6a3b41534e569a470437c8cc28a7e9fe84
vel-3633f <fujiiakito3633@gmail.com>
2026-07-26 13:36:41 +0900

    修正

--- project/oreilly-study/script.js
+++ project/oreilly-study/script.js
@@ -1,20 +1,35 @@
-
-
-// class Range {
-//   constructor(from, to) {
-//     this.from = from;
-//     this.to = to;
+// let Square = class {
+//   constructor(x) {
+//     this.area = x * x;
 //   }
+// };
 
-//   includes(x) {
-//     return this.from <= x && this.to >= x;
-//   }
+// console.log(new Square(3).area);
 
-//   toString() {
-//     return `(${this.from},${this.to})`;
-//   }
-// }
+class Range {
+  constructor(from, to) {
+    this.from = from;
+    this.to = to;
+  }
 
+  includes(x) {
+    return this.from <= x && this.to >= x;
+  }
+
+  toString() {
+    return `(${this.from},${this.to})`;
+  }
+
+  static parse(s) {
+    let matches = s.match(/^\((\d+)\.\.\.(\d+)\)$/);
+    if (!matches) {
+      throw new TypeError(`Cannot parse Range from ""${s}`);
+    }
+    return new Range(parseInt(matches[1]), parseInt(matches[2]));
+  }
+}
+
+console.log(Range.parse("(1...10)"));
 // class Span extends Range {
 //   constructor(start, length) {
 //     if (length >= 0) {
