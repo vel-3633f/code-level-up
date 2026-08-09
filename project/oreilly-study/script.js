d606cabf8a6586ade962bfec20e737a58496fbb9
vel-3633f <fujiiakito3633@gmail.com>
2026-08-09 14:26:23 +0900

    追加

--- project/oreilly-study/script.js
+++ project/oreilly-study/script.js
@@ -1,3 +1,21 @@
+class EZArray extends Array {
+  get first() {
+    return this[0];
+  }
+  get last() {
+    return this[this.length - 1];
+  }
+}
+
+let a = new EZArray();
+console.log(a instanceof EZArray);
+console.log(a instanceof Array);
+a.push(1, 2, 3, 4);
+a.pop();
+console.log(a.first);
+console.log(a.last);
+console.log(a[1]);
+
 // function Range(from, to) {
 //   this.from = from;
 //   this.to = to;
