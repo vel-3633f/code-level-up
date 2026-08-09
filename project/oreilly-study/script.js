c2ba38ce8fcc11f62be72ec40aa9ae0bb25bc143
vel-3633f <fujiiakito3633@gmail.com>
2026-08-09 11:03:00 +0900

    コメントアウト

--- project/oreilly-study/script.js
+++ project/oreilly-study/script.js
@@ -1,60 +1,62 @@
-class Complex {
-  constructor(real, imaginary) {
-    this.r = real;
-    this.i = imaginary;
-  }
 
-  plus(that) {
-    return new Complex(this.r + that.r, this.i + that.i);
-  }
 
-  times(that) {
-    return new Complex(
-      this.r * that.r - this.i * that.i,
-      this.r * that.i + this.i * that.r,
-    );
-  }
+// class Complex {
+//   constructor(real, imaginary) {
+//     this.r = real;
+//     this.i = imaginary;
+//   }
 
-  static sum(c, d) {
-    return c.plus(d);
-  }
-  static product(c, d) {
-    return c.times(d);
-  }
+//   plus(that) {
+//     return new Complex(this.r + that.r, this.i + that.i);
+//   }
 
-  get real() {
-    return this.r;
-  }
-  get imaginary() {
-    return this.i;
-  }
-  get magnitude() {
-    return Math.hypot(this.r, this.i);
-  }
+//   times(that) {
+//     return new Complex(
+//       this.r * that.r - this.i * that.i,
+//       this.r * that.i + this.i * that.r,
+//     );
+//   }
 
-  toString(that) {
-    return `${this.r},${this.i}`;
-  }
+//   static sum(c, d) {
+//     return c.plus(d);
+//   }
+//   static product(c, d) {
+//     return c.times(d);
+//   }
 
-  equals(that) {
-    return that instanceof Complex && this.r === that.r && this.i === that.i;
-  }
-}
+//   get real() {
+//     return this.r;
+//   }
+//   get imaginary() {
+//     return this.i;
+//   }
+//   get magnitude() {
+//     return Math.hypot(this.r, this.i);
+//   }
 
-Complex.ZERO = new Complex(0, 0);
-Complex.ONE = new Complex(1, 0);
-Complex.I = new Complex(0, 1);
+//   toString(that) {
+//     return `${this.r},${this.i}`;
+//   }
 
-let c = new Complex(2, 3);
-let d = new Complex(c.i, c.r);
+//   equals(that) {
+//     return that instanceof Complex && this.r === that.r && this.i === that.i;
+//   }
+// }
 
-console.log(c);
-console.log(d);
-console.log(c.plus(d).toString());
-console.log(c.magnitude);
+// Complex.ZERO = new Complex(0, 0);
+// Complex.ONE = new Complex(1, 0);
+// Complex.I = new Complex(0, 1);
 
-console.log(Complex.product(c, d));
-console.log(Complex.ZERO.toString());
+// let c = new Complex(2, 3);
+// let d = new Complex(c.i, c.r);
+
+// console.log(c);
+// console.log(d);
+// console.log(c.plus(d).toString());
+// console.log(c.magnitude);
+
+// console.log(Complex.product(c, d));
+// console.log(Complex.ZERO.toString());
 
 // class Buffer {
 //   #size = 13;
