1253ca8081e77a35e79373f7b620651075d39624
vel-3633f <fujiiakito3633@gmail.com>
2026-08-24 23:08:02 +0900

    Lampに対してテストを追加

---
+++ project/traffic-light/tests/Lamp.test.js
@@ -0,0 +1,15 @@
+import { expect, test } from 'vitest';
+
+import Lamp from '../js/class/Lamp.js';
+
+test.each(['red', 'yellow', 'green'])('on()を呼ぶと %s が点灯する', (color) => {
+  const lamp = new Lamp(color);
+  lamp.on();
+  expect(lamp.element.style.backgroundColor).toBe(color);
+});
+
+test('off()を呼ぶと、greyに消灯する', () => {
+  const lamp = new Lamp('red');
+  lamp.off();
+  expect(lamp.element.style.backgroundColor).toBe('gray');
+});
